d3b30a731a7bb81599925e43bbcc4fbf70a5157e
kevin olson <acidjazz@gmail.com>
2023-01-21 01:43:14 -0600

    🔧 image uploaded

--- client/pages/inventory/add.vue
+++ client/pages/inventory/add.vue
@@ -3,13 +3,40 @@ useCrumbs().set([
   { name: 'Inventory', to: '/inventory', icon: 'mdi:archive' },
   { name: 'Add an Item', icon: 'mdi:archive-plus' },
 ])
+
+const fileInput = ref<undefined|HTMLInputElement>(undefined)
+
+// prompt to upload a file
+const trigger = () => fileInput?.value?.click()
+
+const handleFileUpload = (e: Event) => {
+  const files = (e.target as HTMLInputElement).files
+  if (files)
+    // do something with the files
+    console.log(files)
+
+}
 </script>
 
 <template>
-  <form-two-column title="Upload Images" description="Start by uploading some pictures of the item">
-    <div class="flex flex-col border border-2 border-dashed border-gray-300 dark:border-gray-700 rounded-xl w-full h-32 flex items-center justify-center">
-      <icon icon="mdi:cloud-upload" class="w-8 h-8 text-gray-300 dark:text-gray-500" />
-      <span>Upload Images</span>
-    </div>
-  </form-two-column>
+  <div class="p-2 lg:p-0">
+    <form-two-column title="Upload Images" description="Start by uploading some pictures of the item">
+      <div
+        class="flex flex-col border border-2 rounded-xl border-dashed
+      border-gray-300 hover:border-gray-500 dark:border-gray-700
+      cursor-pointer w-full h-32 flex items-center justify-center"
+        @click="trigger"
+      >
+        <icon icon="mdi:cloud-upload" class="w-8 h-8 text-gray-300 dark:text-gray-500" />
+        <span>Upload Images</span>
+        <!-- accepts only images -->
+        <input
+          ref="fileInput"
+          type="file" class="hidden" multiple
+          accept="image/*"
+          @change="handleFileUpload"
+        >
+      </div>
+    </form-two-column>
+  </div>
 </template>
